f76810d02e17f99455641e8390126cc6ad303d14
psyc://psyced.org/~lynX <@>
2011-05-20 18:27:21 +0200

    libpsyc glue

---
+++ world/drivers/ldmud/sys/libpsyc.h
@@ -0,0 +1,9 @@
+#ifndef LPC_LIBPSYC_H
+#define LPC_LIBPSYC_H 1
+
+#define PACKET_ROUTING 0	// 2-dimensional mapping of routing modifiers
+#define PACKET_ENTITY  1	// 2-dimensional mapping of entity modifiers
+#define PACKET_METHOD  2	// PSYC method string
+#define PACKET_BODY    3	// packet body.. possibly binary data
+
+#endif
