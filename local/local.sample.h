9b21642db5453455c25a0e6efa41dc02069cf645
psyc://maia.jmt.gr/~jomat <jomat@maia.jmt.gr>
2011-06-13 00:36:13 +0000

    added local.sample.h

---
+++ local/local.sample.h
@@ -0,0 +1,6 @@
+// This is local.h for advanced tunings to the psyced runtime and
+// LPC compilation process. The format looks just like a regular
+// header file for the C language. Settings you can specify
+// here are documented at http://about.psyc.eu/Tuning
+
+#define _userid_nick_mapping    1000: "jomat"
