e30f457e29367392e2fb7af98680a12272060696
jomat <jomat@episkevis.jmt.gr>
2011-06-19 11:20:09 +0000

    added support to add irc: to friends

--- world/net/irc/client_user.c
+++ world/net/irc/client_user.c
@@ -105,87 +105,94 @@ varargs int msg(string source, string mc, string data, mapping vars, int showing
      ,"        loads server settings."\
      ,"        be careful, any unsaved changes will be overwritten"\
     })
-    switch (data) {
-      case "jomat":
-        return 1;
-      case "help":
-      case "?":
-        map(IRC_SCHEME_HELPTEXT,function void(string s) {
-          IRC_SCHEME_ANSWER(s);
-        });
-        break;
-      case "list":
-      case "ls":
-        IRC_SCHEME_ANSWER("Your current serverids: "
-          +implode(m_indices(servers),", "));
-        break;
-      case "list long":
-      case "ll":
-        IRC_SCHEME_ANSWER("Your servers en detail:");
-        map(servers,function void(string s,struct server_s s_s) {
-          IRC_SCHEME_ANSWER("ID "+s);
-          IRC_SCHEME_SHOW_SERVER(s_s);
-        });
-        break;
-      case "save":
-        find_person(owner)->vSet("_irc_servers",save_value(servers));
-        IRC_SCHEME_ANSWER("servers saved");
-        break;
-      case "load":
-        if (catch(servers=restore_value(find_person(owner)->vQuery("_irc_servers")))) {
-          IRC_SCHEME_ANSWER("servers not loaded, try to save some first");
-        } else {
-          IRC_SCHEME_ANSWER("servers loaded");
+    switch (mc) {
+      case "_message_private":
+        switch (data) {
+          case "jomat":
+            return 1;
+          case "help":
+          case "?":
+            map(IRC_SCHEME_HELPTEXT,function void(string s) {
+              IRC_SCHEME_ANSWER(s);
+            });
+            break;
+          case "list":
+          case "ls":
+            IRC_SCHEME_ANSWER("Your current serverids: "
+              +implode(m_indices(servers),", "));
+            break;
+          case "list long":
+          case "ll":
+            IRC_SCHEME_ANSWER("Your servers en detail:");
+            map(servers,function void(string s,struct server_s s_s) {
+              IRC_SCHEME_ANSWER("ID "+s);
+              IRC_SCHEME_SHOW_SERVER(s_s);
+            });
+            break;
+          case "save":
+            find_person(owner)->vSet("_irc_servers",save_value(servers));
+            IRC_SCHEME_ANSWER("servers saved");
+            break;
+          case "load":
+            if (catch(servers=restore_value(find_person(owner)->vQuery("_irc_servers")))) {
+              IRC_SCHEME_ANSWER("servers not loaded, try to save some first");
+            } else {
+              IRC_SCHEME_ANSWER("servers loaded");
+            }
+            break;
+          case "show":
+            IRC_SCHEME_ANSWER("hint: show <server>");
+            break;
+          case "reconnect":
+            //TODO: hmm... don't be so cruel:
+            map(server_connections,function void(string id,object o) {
+              destruct /* see the sad smiley? → */    (o);
+            });  /* ← anotherone */
+            create_connections(1);
+            break;
+          default:
+            if ("show "==data[0..4]) {
+              if (!servers[data[5..]]) {
+                IRC_SCHEME_ANSWER("no such server: "+data[5..]);
+                return 1;
+              }
+              IRC_SCHEME_SHOW_SERVER(servers[data[5..]]);
+            } else if ("add "==data[0..3]) {
+              if (servers[data[4..]]) {
+                IRC_SCHEME_ANSWER("sawwry, this serverid already exists: "+data[4..]);
+                return 1;
+              }
+              servers[data[4..]]=(<server_s>port:6667,nick:owner);
+              IRC_SCHEME_ANSWER(data[4..]+" added");
+            } else if ("del "==data[0..3]) {
+              m_delete(servers,data[4..]);
+              IRC_SCHEME_ANSWER("if "+data[4..]+" was there, now it's gone");
+            } else if ("set "==data[0..3]) {
+              string *sa=explode(data," ");
+              if (sizeof(sa)<3) {
+                IRC_SCHEME_ANSWER("hint: set <server> <property> <value>");
+                return 1;
+              }
+              if (!servers[sa[1]]) {
+                IRC_SCHEME_ANSWER(sa[1]+" does not exits, add it for example");
+                return 1;
+              }
+              switch (sa[2]) {
+                case "host":
+                case "port":
+                case "nick":
+                  IRC_SCHEME_ANSWER(sa[2]+" for "+sa[1]+" is now: "+sa[3]+" was: "+servers[sa[1]]->(sa[2])); 
+                  servers[sa[1]]->(sa[2])=sa[3];
+                  break;
+                default:
+                  IRC_SCHEME_ANSWER(sa[2]+" is currently not a writeable setting");
+              }
+            }
         }
         break;
-      case "show":
-        IRC_SCHEME_ANSWER("hint: show <server>");
+      case "_request_friendship":
+        sendmsg(source,"_notice_friendship_established","Friendship established",vars);
         break;
-      case "reconnect":
-        //TODO: hmm... don't be so cruel:
-        map(server_connections,function void(string id,object o) {
-          destruct /* see the sad smiley? → */    (o);
-        });  /* ← anotherone */
-        create_connections(1);
-        break;
-      default:
-        if ("show "==data[0..4]) {
-          if (!servers[data[5..]]) {
-            IRC_SCHEME_ANSWER("no such server: "+data[5..]);
-            return 1;
-          }
-          IRC_SCHEME_SHOW_SERVER(servers[data[5..]]);
-        } else if ("add "==data[0..3]) {
-          if (servers[data[4..]]) {
-            IRC_SCHEME_ANSWER("sawwry, this serverid already exists: "+data[4..]);
-            return 1;
-          }
-          servers[data[4..]]=(<server_s>port:6667,nick:owner);
-          IRC_SCHEME_ANSWER(data[4..]+" added");
-        } else if ("del "==data[0..3]) {
-          m_delete(servers,data[4..]);
-          IRC_SCHEME_ANSWER("if "+data[4..]+" was there, now it's gone");
-        } else if ("set "==data[0..3]) {
-          string *sa=explode(data," ");
-          if (sizeof(sa)<3) {
-            IRC_SCHEME_ANSWER("hint: set <server> <property> <value>");
-            return 1;
-          }
-          if (!servers[sa[1]]) {
-            IRC_SCHEME_ANSWER(sa[1]+" does not exits, add it for example");
-            return 1;
-          }
-          switch (sa[2]) {
-            case "host":
-            case "port":
-            case "nick":
-              IRC_SCHEME_ANSWER(sa[2]+" for "+sa[1]+" is now: "+sa[3]+" was: "+servers[sa[1]]->(sa[2])); 
-              servers[sa[1]]->(sa[2])=sa[3];
-              break;
-            default:
-              IRC_SCHEME_ANSWER(sa[2]+" is currently not a writeable setting");
-          }
-        }
     }
     return 1;
   }
